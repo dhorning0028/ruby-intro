b3d65de951d2d42f679925bc3dc2fb0e21ccd71c
dhorning0028 <dustin.horning@kellogg.northwestern.edu>
2022-01-27 03:26:58 +0000

    more labs and practice

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -5,5 +5,16 @@
 things_that_would_be_better_with_bacon = ["tacos", "cheeseburgers", "donuts"]
 
 # The Old Way
+puts "bacon #{things_that_would_be_better_with_bacon[0]}"
+puts "bacon #{things_that_would_be_better_with_bacon[1]}"
+puts "bacon #{things_that_would_be_better_with_bacon[2]}"
+
+
 
 # The New Way
+for thing in things_that_would_be_better_with_bacon
+    # do scome code
+    # first time, thing = "tacos"
+    # second time, thing = "cheeseburgers"
+    puts "bacon #{thing}"
+end
